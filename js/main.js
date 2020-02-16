f197606e21405d0284ef69f0177b54e7d16152b1
MayronSouza <mayrondrummer@gmail.com>
2020-02-16 13:02:54 -0300

    Estudo sobre Array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-alert('Meu primeiro JS')
+// PRIMEIRA AULA
+// let name = 'Mayron Souza';
+// let idade = 40;
+// let num1 = 5;
+// let num2 = 3;
+// let frase = 'Japão é o melhor time do mundo';
+
+// alert(name + ' tem ' + idade + ' anos.');
+// console.log(name);
+// console.log(idade);
+// console.log(num1 * num2);
+// console.log(frase.toLowerCase);
+// alert(frase.replace('Japão', 'Brasil'));
+
+// SEGUNDA AULA - Arrray
+let frutas = [{name: 'banana', cor: 'amarela'}, {name: 'laranja', cor: 'verde'}];
+console.log(frutas);
+alert(frutas[1].name);
+
+// let fruta = {name: 'banana', cor: 'amarela'};
+// console.log(fruta);
+
+// let list = ['banana', 'laranja', 'abacate'];
+// list.push('cajú')
+// list.pop()
+// console.log(list.length);
+// console.log(list.toString());
+// console.log(list.join(' | '))
